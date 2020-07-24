4b0224e0653ea5fdb648fc8c4727c5dfa6760d96
Harika <17211a05h0@bvrit.ac.in>
2020-07-24 20:21:34 +0530

    Test Driven Development

---
+++ src/main/java/com/epam/TestDrivenDevelopment/App.java
@@ -0,0 +1,33 @@
+package com.epam.TestDrivenDevelopment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   /* public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }*/
+	public String remove(String s)
+	{
+		if(s.length()==0)
+			return "";
+		else if(s.length()==1)
+		{
+			if(s.charAt(0)=='A')
+				return "";
+			else
+				return s;
+		}
+		String result = "";
+		if(s.charAt(0)!='A')
+			result = result + String.valueOf(s.charAt(0));
+		if(s.charAt(1)!='A')
+			result = result + String.valueOf(s.charAt(1));
+		if(s.length()==2)
+			return result;
+		return result + s.substring(2);
+	}
+}
